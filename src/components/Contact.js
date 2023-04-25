98ae8cd54798d415fa5ae1a668dd4fa9c790f5a6
MasonCathorall <masoncathorallrev@gmail.com>
2023-04-25 18:17:05 -0400

    fix to the package

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,7 +9,6 @@ export const Contact = () => {
     firstName: '',
     lastName: '',
     email: '',
-    phone: '',
     message: ''
   }
   const [formDetails, setFormDetails] = useState(formInitialDetails);
