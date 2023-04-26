faf12db8745c2b9252e9d6418d115d31d90a4112
MasonCathorall <masoncathorallrev@gmail.com>
2023-04-26 15:16:14 -0400

    fixed the warning errors

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,9 +9,34 @@ export const Banner = () => {
     const [isDeleting, setIsDeleting] = useState(false);
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(300 - Math.random() * 100);
-    const [index, setIndex] = useState(1);
+    const [, setIndex] = useState(1);
     const toRotate = [ "Web Developer", "Software Developer", "Software Engineer" ];
     const period = 2000;
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta / 2);
+        }
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setIndex(prevIndex => prevIndex - 1);
+            setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setIndex(1);
+            setDelta(500);
+        } else {
+            setIndex(prevIndex => prevIndex + 1);
+        }
+    }
 
     useEffect(() => {
     let ticker = setInterval(() => {
@@ -19,32 +44,9 @@ export const Banner = () => {
     }, delta);
 
     return () => { clearInterval(ticker) };
-    }, [text])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [tick])
 
-    const tick = () => {
-    let i = loopNum % toRotate.length;
-    let fullText = toRotate[i];
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
-    setText(updatedText);
-
-    if (isDeleting) {
-        setDelta(prevDelta => prevDelta / 2);
-    }
-
-    if (!isDeleting && updatedText === fullText) {
-        setIsDeleting(true);
-        setIndex(prevIndex => prevIndex - 1);
-        setDelta(period);
-    } else if (isDeleting && updatedText === '') {
-        setIsDeleting(false);
-        setLoopNum(loopNum + 1);
-        setIndex(1);
-        setDelta(500);
-    } else {
-        setIndex(prevIndex => prevIndex + 1);
-    }
-    }
 
     return (
         <section className="banner" id="home">
