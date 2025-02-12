7cf64ed693fe58add4034f2ab16a877e3589a37e
MasonCathorall <m.p.cathorall@gmail.com>
2025-02-12 18:14:14 -0500

    Updated some packages

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,6 +9,7 @@ export const Banner = () => {
     const [isDeleting, setIsDeleting] = useState(false);
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(300 - Math.random() * 100);
+    // eslint-disable-next-line
     const [index, setIndex] = useState(1);
     const toRotate = [ "Web Developer", "Software Developer", "Software Engineer" ];
     const period = 2000;
